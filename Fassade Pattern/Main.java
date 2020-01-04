53607856f59c0adbda35a150ca90813564755d4d
Gianmarco Giretti <gianmarco.giretti@htwg-konstanz.de>
2020-01-04 18:49:17 +0100

    simple Fassade Pattern example

---
+++ Fassade Pattern/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        ShapeMaker sm = new ShapeMaker();
+
+        sm.drawCircle();
+        sm.drawReactangle();
+        sm.drawSquare();
+    }
+
+}
